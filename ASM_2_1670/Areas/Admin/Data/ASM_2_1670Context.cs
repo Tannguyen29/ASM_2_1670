7b0e70baa7bc26baeff7a37955d8ea1188c1baa8
Nguyễn Duy Lâm Tùng <tungndlgch210001@fpt.edu.vn>
2023-06-11 20:48:03 +0700

    Category Admin edit

--- ASM_2_1670/Areas/Admin/Data/ASM_2_1670Context.cs
+++ ASM_2_1670/Areas/Admin/Data/ASM_2_1670Context.cs
@@ -17,5 +17,7 @@ namespace ASM_2_1670.Data
         public DbSet<ASM_2_1670.Areas.Admin.Models.User> User { get; set; } = default!;
 
         public DbSet<ASM_2_1670.Areas.Admin.Models.Brand> Brand { get; set; } = default!;
+
+        public DbSet<ASM_2_1670.Areas.Admin.Models.Category> Category { get; set; } = default!;
     }
 }
