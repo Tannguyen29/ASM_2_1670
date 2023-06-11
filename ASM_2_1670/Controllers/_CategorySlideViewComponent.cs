bd0f880a134bdd99f73d80131f45ee4c7b055dd4
VietTan23 <104310603+VietTan23@users.noreply.github.com>
2023-06-11 22:14:37 +0700

    User product

---
+++ ASM_2_1670/Controllers/_CategorySlideViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using ASM_2_1670.Areas.Admin.Models;
+using ASM_2_1670.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ASM_2_1670.Controllers
+{
+    [ViewComponent(Name = "_CategorySlide")]
+    public class _CategorySlideViewComponent:ViewComponent
+    {
+        private readonly ASM_2_1670Context _context;
+
+        public _CategorySlideViewComponent(ASM_2_1670Context context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var _categorySlide = _context.Category.ToList();
+            return View("_CategorySlide", _categorySlide);
+        }
+
+    }
+}
